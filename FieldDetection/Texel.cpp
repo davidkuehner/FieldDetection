cd948acb92f6d202b600c25cc6d264da915447ae
David Kuhner <dav.kuhn@gmail.com>
2014-03-06 14:45:17 +0100

    Add: Texel class implementation

--- FieldDetection/Texel.cpp
+++ FieldDetection/Texel.cpp
@@ -2,11 +2,82 @@
 #include "Texel.h"
 
 
-Texel::Texel(void)
+Texel::Texel(int id, int size)
 {
+	this->id = id;
+	this->width = size;
+	this->height = size;
+	this->zoneId = -1;
+	this->histogram = new int[NB_GRAY];
+	initHistogram();
 }
-
 
 Texel::~Texel(void)
 {
+	delete[] this->histogram;
 }
+
+
+// Private method
+
+void Texel::initHistogram()
+{
+	for( int i = 0 ; i < NB_GRAY ; ++i )
+	{
+		this->histogram[i] = 0;
+	}
+}
+
+
+//Getters and Setters
+
+void Texel::setId(int id)
+{
+	this->id = id;
+}
+
+int Texel::getId()
+{
+	return this->id;
+}
+
+void Texel::setWidth(int width)
+{
+	this->width = width;
+}
+
+int Texel::getWidth()
+{
+	return this->width;
+}
+
+void Texel::setHeight(int height)
+{
+	this->height = height;
+}
+
+int Texel::getHeight()
+{
+	return this->height;
+}
+
+void Texel::setZoneId(int zoneId)
+{
+	this->zoneId = zoneId;
+}
+
+int Texel::getZoneId()
+{
+	return this->zoneId;
+}
+
+void Texel::setHistogram(int* histogram)
+{
+	this->histogram = histogram;
+}
+int* Texel::getHistogram()
+{
+	return this->histogram;
+}
+
+
--- FieldDetection/Texel.h
+++ FieldDetection/Texel.h
@@ -1,8 +1,36 @@
 #pragma once
+
+#define NB_GRAY 256
+
 class Texel
 {
+private:
+	int id;
+	int width;
+	int height;
+	int zoneId;
+	int* histogram;
+
+	void initHistogram();
+
 public:
-	Texel(void);
+	Texel(int id, int size);
 	~Texel(void);
+
+
+	void setId(int id);
+	int getId();
+
+	void setWidth(int width);
+	int getWidth();
+
+	void setHeight(int height);
+	int getHeight();
+
+	void setZoneId(int zoneId);
+	int getZoneId();
+
+	void setHistogram(int* histogram);
+	int* getHistogram();
 };
 
