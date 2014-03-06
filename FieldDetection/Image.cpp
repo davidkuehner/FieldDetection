df45600015525ca8a3521a30b4e4915280db67c7
myshoppinglist <pat18.e@gmail.com>
2014-03-06 14:37:55 +0100

    Impl. of class Image

--- FieldDetection/Image.cpp
+++ FieldDetection/Image.cpp
@@ -2,11 +2,21 @@
 #include "Image.h"
 
 
-Image::Image(void)
+Image::Image(Mat imageInput, int texelSize, double deltaChi2)
 {
-}
+	this->imageInput = imageInput;
+	this->texelSize = texelSize;
+	this->deltaChi2 = deltaChi2;
+	currentRegion = -1;
 
+	initTexels();
+}
 
 Image::~Image(void)
 {
+}
+
+void Image::initTexels()
+{
+	// here : David's work
 }
