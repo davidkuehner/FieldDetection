2fc9f30ec346deb09f0829ba1d8131eaa83f2911
Adrian Moll <moll.adrian@gmail.com>
2014-04-10 15:25:59 +0200

    Treat all images from source folder and saves them in destination folder

--- FieldDetection/Main.cpp
+++ FieldDetection/Main.cpp
@@ -13,8 +13,19 @@ using namespace cv;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Mat mat = FilesTools::getImages("..\\images\\sources\\zones")[0];	
-	FilesTools::showImage(mat);
+	vector<Mat> sourceImages = FilesTools::getImages("..\\images\\sources");
+	vector<Mat> imagesOutput;
+	for(int i = 0; i< sourceImages.size() ; i++)
+	{
+		cout << "Traitement image " << i+1 << endl;
+		Mat mat = sourceImages[i];
+		Image image = Image(mat, 8, 13.0);
+		Mat outputImage = image.getImageOutput();
+		Mat result = FilterTools::imageCombination(image.getImageInput(),outputImage,0.5);
+		imagesOutput.push_back(result);
+	}
+	FilesTools::saveImages(imagesOutput,"..\\images\\destination");
+	/*FilesTools::showImage(mat);
 
 	Image image = Image(mat, 16, 13.0);
 
@@ -25,7 +36,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	Mat result = FilterTools::imageCombination(image.getImageInput(),outputImage,0.5);
 	FilesTools::showImage(result);
 	FilesTools::showImage(outputImage, "result");
-
+	vector<Mat> images;
+	images.push_back(outputImage);
+	FilesTools::saveImages(images,"..\\images\\destination\\zones");*/
 	waitKey();
 	return 0;
 }
